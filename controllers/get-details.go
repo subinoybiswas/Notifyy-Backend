409aa1deaa35e29111ebf58941136f1c1255b488
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-08-11 13:29:40 +0530

    add: send spotify details too

--- controllers/get-details.go
+++ controllers/get-details.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"notifyy.app/backend/helpers"
 );
 
 func GetDetails(c *gin.Context){
@@ -13,9 +15,17 @@ func GetDetails(c *gin.Context){
 			"link":   "https://example.com/article",
 			"heading": "The Evolution of Musicsss",
 		}}
-	// body,_:=helpers.AuthorizeSpotify()
-	// fmt.Printf("Body: %v\n", body.AccessToken)
-	// track,_:=helpers.GetTrackInfo("11dFghVXANMlKmJXsNCbNl",body.AccessToken)
-	// fmt.Printf("Track: %v\n", track.Name)
+	body,_:=helpers.AuthorizeSpotify()
+	fmt.Printf("Body: %v\n", body.AccessToken)
+	track,_:=helpers.GetTrackInfo("11dFghVXANMlKmJXsNCbNl",body.AccessToken)
+	fmt.Printf("Track: %v\n", track.Name)
+
+response["spotify"]=map[string]interface{}{
+		"name": track.Name,
+		"artists": track.Artists,
+		"album": track.Album,
+		"image": track.Album.Images[0].URL,
+		"release_date": track.Album.ReleaseDate,
+}
 	c.JSON(http.StatusOK, response)
 }
