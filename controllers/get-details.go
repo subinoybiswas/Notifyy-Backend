1f8a1773428d09acf6d7a7e78664a836fd6b5f11
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-08-11 18:50:17 +0530

    update: details fetching

--- controllers/get-details.go
+++ controllers/get-details.go
@@ -31,57 +31,58 @@ func FetchDetails() error {
     db := utils.DBConnection()
     defer db.Close()
 
-    // Start a transaction to ensure atomicity
     tx, err := db.Begin()
     if err != nil {
         return err
     }
-    defer tx.Rollback() // Rollback in case of an error
+    defer tx.Rollback()
 
-    // Query to fetch the first unchecked item
     query := `SELECT id, fact, article_link, article_heading, spotify_link 
               FROM details 
-              WHERE checked = 0 
+              WHERE  DATE(date) = DATE('now') 
               LIMIT 1`
-
+	query2 := `SELECT id, fact, article_link, article_heading, spotify_link 
+    		FROM details 
+    		WHERE  checked = 0
+    		LIMIT 1`
     row := tx.QueryRow(query)
 
     var id int
     var fact, articleLink, articleHeading, spotifyLink string
 
-    // Scan the result into variables
     err = row.Scan(&id, &fact, &articleLink, &articleHeading, &spotifyLink)
     if err == sql.ErrNoRows {
-        fmt.Println("No unchecked details found.")
-        
-        // If no unchecked item found, set default values
-        body, _ := helpers.AuthorizeSpotify()
-        DetailsReturn.Spotify, _ = helpers.GetTrackInfo("7MdxDjBTpf7OdwJDnttkc0", body.AccessToken)
-        DetailsReturn.Fact = "Music is a universal language."
-        DetailsReturn.Article.Link = "https://example.com/article"
-        DetailsReturn.Article.Heading = "The Evolution of Music"
 
-        return nil
+        fmt.Println("No unchecked details found.")
+        row = tx.QueryRow(query2)
+		err = row.Scan(&id, &fact, &articleLink, &articleHeading, &spotifyLink)
+		if err == sql.ErrNoRows {
+			body, _ := helpers.AuthorizeSpotify()
+			DetailsReturn.Spotify, _ = helpers.GetTrackInfo("7MdxDjBTpf7OdwJDnttkc0", body.AccessToken)
+			DetailsReturn.Fact = "Music is a universal language."
+			DetailsReturn.Article.Link = "https://example.com/article"
+			DetailsReturn.Article.Heading = "The Evolution of Music"
+			return nil
+		} else if err != nil {
+			return err
+		}
     } else if err != nil {
         return err
     }
 
-    // Update the `checked` field to 1
-    updateStmt := `UPDATE details 
-                   SET checked = 1 
-                   WHERE id = ?`
+updateStmt := `UPDATE details 
+               SET checked = 1, date = DATE('now') 
+               WHERE id = ?`
 
     _, err = tx.Exec(updateStmt, id)
     if err != nil {
         return err
     }
 
-    // Commit the transaction
     if err := tx.Commit(); err != nil {
         return err
     }
 
-    // If `spotifyLink` is not empty, fetch additional Spotify track info
     if spotifyLink != "" {
 		link,err:=helpers.ExtractTrackID(spotifyLink)
 		if err!=nil{
@@ -92,7 +93,6 @@ func FetchDetails() error {
         DetailsReturn.Spotify = track
     }
 
-    // Set the fetched details to `DetailsReturn`
     DetailsReturn.Fact = fact
     DetailsReturn.Article.Link = articleLink
     DetailsReturn.Article.Heading = articleHeading
@@ -104,23 +104,6 @@ func FetchDetails() error {
 
 func GetDetails(c *gin.Context){
 	
-	// response:=map[string]interface{}{
-	// 	"fact": "Music is a universal language.",
-	// 	"article": map[string]interface{}{
-	// 		"link":   "https://example.com/article",
-	// 		"heading": "The Evolution of Musicsss",
-	// 	}}
-	// body,_:=helpers.AuthorizeSpotify()
-	// track,_:=helpers.GetTrackInfo("11dFghVXANMlKmJXsNCbNl",body.AccessToken)
-	// fmt.Printf("Track: %v\n", track.Name)
-
-	// response["spotify"]=map[string]interface{}{
-	// 	"name": track.Name,
-	// 	"artists": track.Artists,
-	// 	"album": track.Album,
-	// 	"image": track.Album.Images[0].URL,
-	// 	"release_date": track.Album.ReleaseDate,
-	// }
 	if err:=FetchDetails();err!=nil{
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Internal Server Error",
